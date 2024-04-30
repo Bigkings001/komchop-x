4b80e487bff2712a47eaa15b59bf1771a34dc5c7
akom <akom.victory@gmail.com>
2024-04-30 01:16:20 +0100

    resolve static files

--- foodOnline_main/static/js/bootstrap.js
+++ foodOnline_main/static/js/bootstrap.js
@@ -1524,7 +1524,7 @@ if (typeof jQuery === 'undefined') {
     this.init('popover', element, options)
   }
 
-  if (!$.fn.tooltip) throw new Error('Popover requires tooltip.js')
+  if (!$.fn.tooltip) throw new Error('Popover%20requires%20tooltip.html')
 
   Popover.VERSION  = '3.3.7'
 
